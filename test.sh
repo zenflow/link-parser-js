248678b3ef812e7912199ceb98f26e3041e6f65b
Matthew Francis Brunetti <zenflow87@gmail.com>
2020-03-26 03:48:47 +0200

    Add package.json

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@ set -ex
 
 pushd "$(dirname "$0")"
 
-echo The needs of the many outweigh the needs of the few. | node dist/link-parser.js
+npm link
+echo The needs of the many outweigh the needs of the few. | link-parser-js
+npm unlink
 
 popd
