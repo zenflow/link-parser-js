69836dc2b24da535e836ba8cbb8b031cee43be6b
Matthew Francis Brunetti <zenflow87@gmail.com>
2021-03-16 02:11:18 -0400

    Upgrade link-grammar to latest commit (dcec4933e784368bde731907848ce09c81207458)

--- scripts/install_link-grammar.sh
+++ scripts/install_link-grammar.sh
@@ -4,8 +4,9 @@ set -ex
 # Change directory to project root
 pushd "$(dirname "$0")/.."
 
-git clone -b link-grammar-5.8.0 https://github.com/opencog/link-grammar link-grammar
+git clone https://github.com/opencog/link-grammar link-grammar
 pushd link-grammar
+git checkout dcec4933e784368bde731907848ce09c81207458
 ./autogen.sh
 popd
 
